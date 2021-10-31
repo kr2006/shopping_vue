d3b710662a91aa4c6e5c90553ac0eb52a7a4cb21
kr2006 <sasakravec663@gmail.com>
2021-10-31 15:48:37 +0200

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pt-14 pa-6 mb-2">
+    <h1>This is About Page of my project</h1>
+    <h2 class="mb-4">Welcome!</h2>
+    <div class="py-2">
+      Welcome to my project! Here you can find a lot of information about myself
+      and how to create simple vue project. Stay tuned!
+    </div>
   </div>
 </template>
---
+++ src/views/List.vue
@@ -0,0 +1,81 @@
+<template>
+      <v-list
+      subheader
+      flat
+    >
+     <v-text-field class='ma-6'
+            v-model="newItemTitle"
+            clearable
+            @keyup.enter='addNewItem()'
+            outlined
+            @click:append="addNewItem()"
+            label="Add input"
+            append-icon="mdi-cart-outline"
+          ></v-text-field>
+    <div v-for="(item, id) in shoppingList"
+        :key="item.title">
+        <v-list-item
+          @click="doneBought(item.id)"
+          :class="{ 'blue lighten-5' : item.bought}"
+        >
+          <template>
+            <v-list-item-action>
+              <v-checkbox
+                :input-value='item.bought'
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title :class="{'text-decoration-line-through' : item.bought}">
+              {{item.title}}
+              </v-list-item-title>
+            </v-list-item-content>
+             <v-list-item-action>
+                    <v-btn icon
+                     @click="deleteItem(id)">
+                      <v-icon color="grey lighten-1">mdi-delete</v-icon>
+                    </v-btn>
+              </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider inset>
+        </v-divider>
+    </div>
+    </v-list>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  components: {
+  },
+  data: () => ({
+    shoppingList: [
+      { id: 0, title: 'Banana', bought: false },
+      { id: 1, title: 'Pear', bought: false },
+      { id: 2, title: 'Onion', bought: false }
+    ],
+    newItemTitle: ' '
+  }),
+  methods: {
+    doneBought (id) {
+      console.log(this.shoppingList.filter(item => item.id === id))
+      const itemList = this.shoppingList.filter(item => item.id === id)
+      itemList[0].bought = !itemList[0].bought
+    },
+    deleteItem (id) {
+      this.shoppingList.splice(id, 1)
+    },
+    addNewItem () {
+      this.shoppingList.push({
+        id: this.shoppingList.length,
+        title: this.newItemTitle,
+        bought: false
+      })
+      this.newItemTitle = ' '
+      this.shoppingList.forEach((i) => { console.log(i.id) })
+    }
+  }
+}
+</script>
